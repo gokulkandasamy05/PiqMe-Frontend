5b82c7114cc9ecbe18f3f8a0513aa815ee9f6a74
Gokul <85794806+gokulkandasamy05@users.noreply.github.com>
2025-08-08 21:49:13 +0530

    bugs

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,31 @@
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
+// middleware.ts
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { persistor } from './utils/appStore';
 
-// export function middleware(request: NextRequest) {
-//   const token = request.cookies.get('token')?.value;
-//   const { pathname } = request.nextUrl;
+export function middleware(request: NextRequest) {
+  const userCookie = request.cookies.get('token');
+  const isLoggedIn = !!userCookie?.value;
+  const { pathname } = request.nextUrl;
 
-//   const isLoggedIn = !!token;
+  const protectedPaths = ['/', '/feed'];
+  const isProtected = protectedPaths.some((path) =>
+    pathname === path || pathname.startsWith(path + '/')
+  );
+  
 
-//   const isProtectedPath = ['/feed', '/'].includes(pathname);
+  if (isProtected && !isLoggedIn) {
+    persistor.purge()
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
-//   // Redirect to login if not logged in and accessing a protected route
-//   if (!isLoggedIn && isProtectedPath) {
-//     return NextResponse.redirect(new URL('/login', request.url));
-//   }
+  if (pathname === '/' && isLoggedIn) {
+    return NextResponse.redirect(new URL('/feed', request.url));
+  }
 
-//   // Prevent logged-in users from visiting /login or /
-//   if (isLoggedIn && (pathname === '/login' || pathname === '/')) {
-//     return NextResponse.redirect(new URL('/feed', request.url));
-//   }
+  if(isLoggedIn && pathname === '/login'){
+    return NextResponse.redirect(new URL('/feed', request.url));
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
