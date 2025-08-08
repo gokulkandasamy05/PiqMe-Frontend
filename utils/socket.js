9cd17b3f2f24c70b64d4382ac40e3c202d2f1ab2
Gokul <85794806+gokulkandasamy05@users.noreply.github.com>
2025-08-08 14:29:28 +0530

    chat

--- utils/socket.js
+++ utils/socket.js
@@ -1,5 +1,8 @@
 import io from 'socket.io-client'
 
 export const createSocketConnection = () => {
-    return io(process.env?.NEXT_PUBLIC_API_BASE_URL)
+    return io(process.env?.NEXT_PUBLIC_API_BASE_URL, {
+        transports: ['websocket', 'polling'], // important for fallback
+        withCredentials: true
+    })
 }
