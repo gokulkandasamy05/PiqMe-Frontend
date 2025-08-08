f7aded64189a1ab4f49d3d1fd14065e10b205767
Gokul <85794806+gokulkandasamy05@users.noreply.github.com>
2025-08-08 14:19:46 +0530

    chat

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   // other config options...
   images: {
-    domains: ['localhost','52.91.177.135'],
+    domains: ['localhost','52.91.177.135','piqme.live'],
   },
 };
 
