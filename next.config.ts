fdc3073a49f64ad2fe9c6a9b22cdc98eaa248970
Gokul <85794806+gokulkandasamy05@users.noreply.github.com>
2025-08-13 12:44:15 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -6,21 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['localhost', '52.91.177.135', 'piqme.live'],
   },
-  // Optional: If you’re deploying behind a reverse proxy like NGINX
-  // and want to make sure middleware behaves correctly
-  async headers() {
-    return [
-      {
-        source: '/(.*)', // applies to all routes
-        headers: [
-          {
-            key: 'X-Forwarded-Proto',
-            value: 'https',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
