84834d2446f97d194fe63db7f062d09332c2281d
Gokul <85794806+gokulkandasamy05@users.noreply.github.com>
2025-08-20 16:15:26 +0530

    changes

--- utils/socket.js
+++ utils/socket.js
@@ -4,7 +4,7 @@ export const createSocketConnection = () => {
   if (location.hostname === 'localhost') {
     return io(process.env.NEXT_PUBLIC_API_BASE_URL);
   }else{
-    io('/', {
+    return io('/', {
       path: "/api/socket.io",
       transports: ["websocket"],
       withCredentials: true
